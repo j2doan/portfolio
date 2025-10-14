086128aad849ae9ab0845a634824fc7872ff01e4
j2doan <j2.doan@san.rr.com>
2025-10-14 01:05:03 -0700

    added highlighting and opening new tabs in JS

--- global.js
+++ global.js
@@ -1,7 +1,7 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
 }
 
 // let navLinks = $$("nav a");
@@ -14,18 +14,33 @@ function $$(selector, context = document) {
 // currentLink?.classList.add('current');
 
 let pages = [
-  { url: '', title: 'Home' },
-  { url: 'projects/', title: 'Projects' },
-  { url: 'contact/', title: 'Contact' },
-  { url: 'resume/', title: 'Resume' },
-  { url: 'https://github.com/j2doan', title: 'Profile' },
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/j2doan', title: 'Profile' },
 ];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let url = p.url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    let title = p.title;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    a.classList.toggle(
+        "current",
+        a.host === location.host && a.pathname === location.pathname
+    );
+    a.toggleAttribute("target", a.host !== location.host);
+
+    nav.append(a);
 }
